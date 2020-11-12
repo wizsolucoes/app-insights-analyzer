f313d5e76eec8a298e4cc90719e6225a49ca59a0
Touré Holder <toureh@hotmail.com>
2020-11-12 18:33:09 -0300

    chore: melhorar mensagem de falha

--- src/build-breaker.js
+++ src/build-breaker.js
@@ -8,7 +8,7 @@ exports.run = function (instrumentationKey, result) {
 
   if (!result) {
     handleError(
-      `Expected Application Insights to be configured with instrumentation key: ${instrumentationKey}.`
+      `Expected Application Insights to be configured with instrumentation key: ${instrumentationKey}.\n\nSiga as orientações no nosso knowledge base para fazer a configuração:\n - https://parcorretoradeseguros.sharepoint.com/sites/Gov.Met.Devz/SitePages/Chave-de-instrumenta%C3%A7%C3%A3o-Application-Insights---Onde-encontrar.aspx\n - https://parcorretoradeseguros.sharepoint.com/sites/Gov.Met.Devz/SitePages/Chave-de-instrumenta%C3%A7%C3%A3o-Application-Insights---Onde-encontrar.aspx`
     );
   } else {
     console.log('INFO:', 'Application Insights is configured as expected.');
